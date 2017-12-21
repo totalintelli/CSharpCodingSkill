930b7079987540997486f435898cbf1ba8725e6a
DEVZONEYDS\USER <kwanwoo5@naver.com>
2017-12-21 21:22:28 +0900

    first commit

---
+++ CheckedContext/CheckedContext/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckedContext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            checked
+            {
+                const char AlphaA = 'a';
+                for(byte i = 0; i < 255; i++)
+                {
+                    if (i + 0x20 == AlphaA) Console.WriteLine("match 'A'!");
+                }
+            }
+        }
+    }
+}
